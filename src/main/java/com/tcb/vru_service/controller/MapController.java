0eec7e05f946973d53c0b5fe5f3a0208f694790e
issacwanglei <wanglei3178@163.com>
2020-09-04 11:36:20 +0800

    修改默认时间

--- src/main/java/com/tcb/vru_service/controller/MapController.java
+++ src/main/java/com/tcb/vru_service/controller/MapController.java
@@ -6,8 +6,10 @@ import com.tcb.vru_service.response.ResultVO;
 import com.tcb.vru_service.service.IMapService;
 import com.tcb.vru_service.util.CommonFunction;
 import com.tcb.vru_service.util.DateUtil;
+import com.tcb.vru_service.util.DefaultParameter;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -41,12 +43,24 @@ public class MapController {
      * @return
      */
     @PostMapping(value = "getInstitutionData")
-    public ResultVO<PointDataVO> getInstitutionData(String institutionId, String beginTime, String endTime, HttpServletRequest request) {
+    public ResultVO<PointDataVO> getInstitutionData(
+            String institutionId, @RequestParam(required = false) String beginTime,
+            @RequestParam(required = false) String endTime, HttpServletRequest request) {
         ResultVO<PointDataVO> resultVO;
-        if (!StringUtils.isEmpty(institutionId) && !StringUtils.isEmpty(beginTime) && !StringUtils.isEmpty(endTime)) {
+        if (!StringUtils.isEmpty(institutionId)) {
+            Timestamp beginTimestamp;
+            Timestamp endTimestamp;
+            if (!StringUtils.isEmpty(beginTime)) {
+                beginTimestamp = DateUtil.StringToTimestamp(beginTime, DateUtil.DATA_TIME_SECOND);
+            } else {
+                beginTimestamp = DateUtil.GetSystemDateTime(DefaultParameter.MILLISECOND_ONE_HOUR * 24);
+            }
+            if (!StringUtils.isEmpty(endTime)) {
+                endTimestamp = DateUtil.StringToTimestamp(endTime, DateUtil.DATA_TIME_SECOND);
+            } else {
+                endTimestamp = DateUtil.GetSystemDateTime(0);
+            }
             String userCode = CommonFunction.getLoginUserCode(request);
-            Timestamp beginTimestamp = DateUtil.StringToTimestamp(beginTime, DateUtil.DATA_TIME_SECOND);
-            Timestamp endTimestamp = DateUtil.StringToTimestamp(endTime, DateUtil.DATA_TIME_SECOND);
             PointDataVO pointDataVO = mapService.getPointData(Long.valueOf(institutionId), userCode, beginTimestamp, endTimestamp);
             if (pointDataVO != null) {
                 resultVO = new ResultVO(pointDataVO);
